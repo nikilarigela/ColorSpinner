eba5870ba609856a914d4cd8e78fe393434835d6
nikilarigela <nikil.arigela@gmail.com>
2016-07-26 19:45:04 +0530

    instead of circle i'm using square removed images

--- app/src/main/java/com/srrdnew/user/colorspinner/MainActivity.java
+++ app/src/main/java/com/srrdnew/user/colorspinner/MainActivity.java
@@ -2,25 +2,48 @@ package com.srrdnew.user.colorspinner;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    int redColorCode = 0xff0000;
+    int greenColorCode = 0x00ff00;
+    int blueColorCode = 0x0000ff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<ItemData> list=new ArrayList<>();
-        list.add(new ItemData("Red",R.drawable.r));
-        list.add(new ItemData("Green",R.drawable.g));
-        list.add(new ItemData("Blue",R.drawable.b));
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+        CharacterDrawable drawable = new CharacterDrawable('A', 0xFF805781);
+        image.setImageDrawable(drawable);
 
-        Spinner sp=(Spinner)findViewById(R.id.spinner);
-        SpinnerAdapter adapter=new SpinnerAdapter(this,
-                R.layout.spinner_layout,R.id.txt,list);
+
+        ArrayList<ItemData> list = new ArrayList<>();
+        list.add(new ItemData("Red", redColorCode));
+        list.add(new ItemData("Green", greenColorCode));
+        list.add(new ItemData("Blue", blueColorCode));
+
+        final Spinner sp = (Spinner) findViewById(R.id.spinner);
+        SpinnerAdapter adapter = new SpinnerAdapter(this,
+                R.layout.spinner_layout, R.id.txt, list);
         sp.setAdapter(adapter);
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                //Our code if item selected
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
 
--- app/src/main/java/com/srrdnew/user/colorspinner/SpinnerAdapter.java
+++ app/src/main/java/com/srrdnew/user/colorspinner/SpinnerAdapter.java
@@ -16,6 +16,8 @@ import java.util.ArrayList;
  */
 public class SpinnerAdapter extends ArrayAdapter<ItemData> {
     int groupid;
+    char letter;
+    int color;
     Activity context;
     ArrayList<ItemData> list;
     LayoutInflater inflater;
@@ -31,10 +33,27 @@ public class SpinnerAdapter extends ArrayAdapter<ItemData> {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         View itemView = inflater.inflate(groupid, parent, false);
-        ImageView imageView = (ImageView) itemView.findViewById(R.id.img);
-        imageView.setImageResource(list.get(position).getImageId());
         TextView textView = (TextView) itemView.findViewById(R.id.txt);
         textView.setText(list.get(position).getText());
+        /**getting the text to string
+         * retriving the first letter
+         * getting the color code
+         * retriving the color code
+         * implement in CharacterDrawable
+         **/
+        String s = list.get(position).getText().toString();
+        for (int i = 0; i < 1; i++)
+        {
+            // Get char at this index and display it.
+            letter = s.charAt(i);
+            System.out.println(letter);
+        }
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.img);
+        color = list.get(position).getImageId();
+        System.out.println(color);
+        CharacterDrawable drawable = new CharacterDrawable(letter,color);
+        imageView.setImageDrawable(drawable);
+
         return itemView;
     }
 
